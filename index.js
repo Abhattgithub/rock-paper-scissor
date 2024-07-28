cdf27cb267fb64fc0c7112ce1e4d4eab4c1f932b
Abhijeet Bhatt <65093661+Abbhattgithub@users.noreply.github.com>
2024-07-28 20:35:20 +0530

    Added button for user choice, Added Scoreboard

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-function playGame(){
-    for(let i = 0; i<5; i++){
-        let playerChoice = getPlayerChoice(); 
-        let computerChoice = getComputerChoice();
-        console.log(`-Round ${i+1}-`);
-        gameResult(computerChoice, playerChoice)
-    }
+let playerChoiceStone = querySelector("#stoneBtn");
+let playerChoicePaper = querySelector("#paperBtn");
+let playerChoiceScissor = querySelector("#scissorBtn");
+let roundNumber = 0;
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+
+let displayRoundNumber = querySelector("#displayRoundNumber")
+let displayRoundWinner = querySelector("#displayRoundWinner")
+let displayPlayerChoice = querySelector("#displayPlayerChoice")
+let displayPlayerScore = querySelector("#displayPlayerScore")
+let displayComputerChoice = querySelector("#displayComputerChoice")
+let displayComputerScore = querySelector("#displayComputerScore")
+
+
+playerChoiceStone.addEventListner("click", ()=> {
+    playerChoice = "STONE"
+    playgame()
+} )
+playerChoicePaper.addEventListner("click", ()=> {
+    playerChoice = "PAPER"
+    playgame()
+} )
+playerChoiceScissor.addEventListner("click", ()=> {
+    playerChoice = "SCISSOR"
+    playgame()
+} )
+async function playGame(){
+    await buttonpressed;
+    let computerChoice = getComputerChoice();
+    roundNumber++;
+    gameResult(computerChoice, playerChoice)
 }
 
 //Randomly selects computers choice using Math.random
@@ -14,28 +40,45 @@ function getComputerChoice(){
     return choices[computerChoice]
 }
 
-//Inputs player's choice using prompt
-function getPlayerChoice(){
-    let playerChoice = prompt("Enter your choice (Rock | Paper | Scissor)");
-    playerChoice = playerChoice.toUpperCase()
-    return playerChoice
-}
-
 //Comparing the player choice and computer's choice to see the result
 function gameResult(computerChoice, playerChoice){
-    if(playerChoice == computerChoice)
-    console.log(`Player choice: ${playerChoice}
-Computer choice: ${computerChoice}
-Tie!!`)
-else if(playerChoice=="ROCK"&&computerChoice=="PAPER" || playerChoice=="PAPER"&&computerChoice=="SCISSOR" ||playerChoice=="SCISSOR"&&computerChoice=="ROCK")
-console.log(`Player choice: ${playerChoice}
-Computer choice: ${computerChoice}
-You lost!!`) 
-else if(playerChoice=="ROCK"&&computerChoice=="SCISSOR" || playerChoice=="PAPER"&&computerChoice=="ROCK" ||playerChoice=="SCISSOR"&&computerChoice=="PAPER")
-console.log(`Player choice: ${playerChoice}
-Computer choice: ${computerChoice}
-You Won!!`)
-else
-console.log("Try Again!!! Invalid Input ");
+    displayRoundNumber.innerHTML() = roundNumber
+    while(computerScore < 5 && playerScore < 5){
+    if(playerChoice == computerChoice){
+        result = "TIE"
+        updateBoard()
+        playGame()
+    }
+    else if(playerChoice=="ROCK"&&computerChoice=="PAPER" || playerChoice=="PAPER"&&computerChoice=="SCISSOR" ||playerChoice=="SCISSOR"&&computerChoice=="ROCK"){
+        result = "YOU LOST"
+        computerScore++
+        updateBoard()
+        playGame()
+    }
+    else if(playerChoice=="ROCK"&&computerChoice=="SCISSOR" || playerChoice=="PAPER"&&computerChoice=="ROCK" ||playerChoice=="SCISSOR"&&computerChoice=="PAPER"){
+        result = "YOU WON"
+        playerScore++;
+        updateBoard()
+        playGame()
+    }
+    else
+        console.log("Try Again!!! Invalid Input ");
+    }
+    if(playerScore == 5){
+        result = `YOU WON
+        Game Ends`
+        updateBoard()
+    } else {
+        result = `YOU LOST
+        Game Ends`
+        updateBoard()
+    }
 }
-playGame()
+
+function updateBoard(result){
+    displayRoundWinner.innerHTML() = result
+    displayComputerChoice.innerHTML() = computerChoice
+    displayPlayerChoice.innerHTML() = playerChoice
+    displayComputerScore.innerHTML() = computerScore
+    displayPlayerScore.innerHTML() = playerScore
+}
